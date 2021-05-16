12a1f1abd848c1ccf2b4798c2ec874b2440f3ba4
nitishkumar07 <nitishkumarj.g07@gmail.com>
2021-05-16 10:47:57 +0530

    login and signin

---
+++ login/login.js
@@ -0,0 +1,36 @@
+let email = document.querySelector("#email");
+let pw= document.querySelector("#pass");
+let submitbtn= document.querySelector(".submitbtn");
+let registerbtn=document.querySelector(".registerbtn");
+let message=document.querySelector(".message");
+let loginbtn=document.querySelector(".loginbtn");
+
+window.addEventListener("load",function(){
+    submitbtn.addEventListener("click",submitbtnHandler);
+    registerbtn.addEventListener("click",registerbtnHandler);
+
+})
+function registerbtnHandler(){
+    window.location.href="/home/nitish/Desktop/Campuscanteen/register/register.html";
+
+}
+async function submitbtnHandler(e){
+    try{
+        e.preventDefault();
+        if(email.value && pw.value){
+        console.log(email.value,pw.value);
+        let obj = await axios.post( "https://foodcolony.herokuapp.com/user/login" , {email:email.value , password:pw.value});
+        console.log(obj);
+        if(obj.data.data){
+            window.location.href="/menu";
+
+        }
+        else{
+            message.innerHTML=obj.data.message;
+
+        }
+    }
+    }catch(error){
+        HTMLFormControlsCollection.log(error);
+    }
+}
